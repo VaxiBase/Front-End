558debdcbb62c30bdceaba78e3e0f2ff11c6f479
Michael Arabian <michaelarabian@Michaels-MacBook-Air.local>
2021-10-15 14:39:59 -0400

    added popup

--- vaxibase/src/App.js
+++ vaxibase/src/App.js
@@ -1,26 +1,39 @@
 
 import './App.css';
 
-import React from "react"
+import React,{useState} from "react"
 import CanadaMap from "react-canada-map"
+import Popup from 'react-animated-popup'
+
+
+
 
 function App() {
+
   const mapClickHandler = (province, event) => {
-    alert(province + "  Vaccinations : 25 5040")
+    setMessage(province)
+    setVisible(true)
   }
 
+  const [visible, setVisible] = useState(false)
+  const [message, setMessage] = useState(" ")
  
 
   return (
     <div className="page">
-      <div className = "map">
-          <CanadaMap
-            fillColor="white"
-            onHoverColor="Blue"
+        <div className = "map">
+            <CanadaMap
+              fillColor="white"
+              onHoverColor="Blue"
+              onClick = {mapClickHandler}
             
-            onHover={mapClickHandler}
-          ></CanadaMap>
-      </div>
+            ></CanadaMap>
+        </div>
+        <div className = "popup">
+            <Popup visible={visible} onClose={() => setVisible(false)}>
+                <p>{message}</p>
+            </Popup>
+        </div>
     </div>
 
   )
